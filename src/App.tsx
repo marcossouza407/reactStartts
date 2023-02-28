d6b2040ea3cd10dbb90e3e4150fe2e851b0b6db0
marcossouza407 <116312153+marcossouza407@users.noreply.github.com>
2023-02-28 13:58:40 -0300

    ft post, Avatar tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,13 @@
 import { Sidebar } from "./components/Sidebar";
 import { Header } from "./components/Header"
-import { Post } from "./components/Post"
+import { Post,PostType } from "./components/Post"
 
 
 import styles from './App.module.css';
 
 import './global.css';
 
-const posts = [
+const posts: PostType[] = [
   {
     id: 1,
     author:{
@@ -57,9 +57,7 @@ export function App() {
         return (
         <Post
         key={post.id}
-        author={post.author}
-        content={post.content}
-        publishedAt={post.publishedAt}
+        post={post}
         />
         )
       })}
